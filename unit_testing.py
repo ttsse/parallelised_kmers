2ca70cb63a6a3eff5392e865a72ff5b24f584608
dibsondivya <divyashridar14@gmail.com>
2025-07-14 18:01:18 +0200

    added model training test

--- unit_testing.py
+++ unit_testing.py
@@ -1,11 +1,22 @@
 import unittest
+import os
 from data_modelling import results_gen
 
 class TestDataImport(unittest.TestCase):
     def test_fake_data(self):
+        case = results_gen('all', '22', 'absence')
         with self.assertRaises(FileNotFoundError):
-            results_gen('all', '22', 'absence')
+            case.dataImport()
     def test_real_data(self):
-        self.assertIsNone(results_gen('all', '17', 'absence'))
-            
+        case = results_gen('all', '17', 'absence')
+        self.assertIsNotNone(case.dataImport())
+    def test_model_output_produced(self):
+        k = '17'
+        predicting = 'absence'
+        kmer = 'all'
+        method = 'l2'
+        file_path = os.getcwd() + '/models/confusion_matrix/'+ 'k'+k+'_'+kmer+'_'+predicting+'_'+method+'.png'
+        case = results_gen(kmer, k, predicting)
+        case.modelTraining()
+        assert os.path.isfile(file_path)
 unittest.main()
