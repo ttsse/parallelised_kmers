3efc8c1a966442caf8169fda58bdc6806638fecc
dibsondivya <divyashridar14@gmail.com>
2025-07-16 13:31:15 +0200

    bug fix, add comments

--- unit_testing.py
+++ unit_testing.py
@@ -12,25 +12,36 @@ def disable_DeprecationWarning(fn):
 
 class TestData(unittest.TestCase):
     @disable_DeprecationWarning
+    
+    # test for spurious input for file naming convention
     def test_fake_data(self):
         case = results_gen('all', '22', 'absence')
         with self.assertRaises(FileNotFoundError):
             case.dataImport()
+
+    # test for valid file naming input
     def test_real_data(self):
         case = results_gen('all', '17', 'absence')
         self.assertIsNotNone(case.dataImport())
+
+    # test for successful model training given valid file
     def test_model_no_error(self):
         case = results_gen('all', '17', 'absence')
         self.assertIsNotNone(case.modelTraining())
+
+    # test for error raised in model training given spurious file name input
     def test_model_error(self):
         case = results_gen('all', '22', 'absence')
         with self.assertRaises(FileNotFoundError):
             case.modelTraining()
+
+    # test for accurate model output produced
+        # divya comment: ok maybe this test doesnt really make sense,,, why would the file be randomly named something else,,,
     def test_model_output_not_produced(self):
         k = '17'
         predicting = 'absence'
         kmer = 'all'
-        method = 'l2'
+        method = 'svm'
         fake_k = '50'
         cross_validation_results_file_path = os.getcwd() + '/models/hyperparams/' + 'k'+fake_k+'_'+kmer+'_'+predicting+'_'+method+'.csv'
         classification_results_file_path = os.getcwd() + '/models/classification_report/' + 'k'+fake_k+'_'+kmer+'_'+predicting+'_'+method+'.csv'
@@ -44,11 +55,12 @@ class TestData(unittest.TestCase):
         self.assertFalse(os.path.isfile(confusion_matrix_file_path))
         self.assertFalse(os.path.isfile(model_file_path))
 
+    # test for accurate model output produced
     def test_model_output_produced(self):
         k = '17'
         predicting = 'absence'
         kmer = 'all'
-        method = 'l2'
+        method = 'svm'
         cross_validation_results_file_path = os.getcwd() + '/models/hyperparams/' + 'k'+k+'_'+kmer+'_'+predicting+'_'+method+'.csv'
         classification_results_file_path = os.getcwd() + '/models/classification_report/' + 'k'+k+'_'+kmer+'_'+predicting+'_'+method+'.csv'
         confusion_matrix_file_path = os.getcwd() + '/models/confusion_matrix/'+ 'k'+k+'_'+kmer+'_'+predicting+'_'+method+'.png'
@@ -61,10 +73,12 @@ class TestData(unittest.TestCase):
         self.assertTrue(os.path.isfile(confusion_matrix_file_path))
         self.assertTrue(os.path.isfile(model_file_path))
 
+    # test for accurate performance metric recording: valid values are non-zero and positive
     def test_performance_recording(self):
         case = results_gen('all', '17', 'absence')
         case.modelTraining()
         training_time, max_training_memory = case.performanceMetrics()
         self.assertTrue(training_time>0)
         self.assertTrue(max_training_memory>0)
+
 unittest.main()
