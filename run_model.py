5d0f588885b6080940ab1315fc7c3705e3276679
Divya Shridar <56643379+dibsondivya@users.noreply.github.com>
2025-08-22 15:16:49 +0200

    path update

--- run_model.py
+++ run_model.py
@@ -2,15 +2,13 @@ import subprocess
 from multiprocessing.dummy import Pool
 
 
-#kmer_list = ['all', 'kmer']
-#k_list = ['17', '21', '31']
-#predicting_list = ['absence', 'sumcount', 'meancount', 'mediancount']
+kmer_list = ['all', 'kmer']
+k_list = ['17', '21', '31']
+predicting_list = ['absence', 'sumcount', 'meancount', 'mediancount']
 
-kmer_list = ['all']
-k_list = ['17']
-predicting_list = ['absence']
-
-command_list = [f"import os; os.chdir('/Users/vaish555/Documents/Uppsala Universitet/(1) courses/(12) TTSSE/ttsse/'); from data_modelling import results_gen; results_gen('{km}','{k}','{p}').modelTraining()" for km in kmer_list for k in k_list for p in predicting_list]
+# add filepath to current directory
+#command_list = [f"import os; os.chdir('/Users/vaish555/Documents/Uppsala Universitet/(1) courses/(12) TTSSE/ttsse/'); from data_modelling import results_gen; results_gen('{km}','{k}','{p}').modelTraining()" for km in kmer_list for k in k_list for p in predicting_list]
+command_list = [f"import os; os.chdir('$FILEPATH'); from data_modelling import results_gen; results_gen('{km}','{k}','{p}').modelTraining()" for km in kmer_list for k in k_list for p in predicting_list]
 
 def run_model(x):
     subprocess.call(["python3", "-c", x])
